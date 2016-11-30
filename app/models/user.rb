2e96cd1478998f58ccc25d63b05ce075544834df
Ana Claudia <ana@anaclaudia.net.br>
2016-11-30 18:35:44 +0100

    Removing website presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
 
-  validates(:firstname, presence: true, lenght: { maximum: 30 })
-  validates(:lastname, presence: true, lenght: { maximum: 30 })
-  validates(:website, length: { maximum: 255 })
+  validates(:firstname, presence: true, length: { maximum: 30 })
+  validates(:lastname, presence: true, length: { maximum: 30 })
 
 end
