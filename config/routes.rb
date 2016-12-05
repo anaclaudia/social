1fea9a3d41d09fda77be0da16bf6df0c30ebf243
Ana Claudia <ana@anaclaudia.net.br>
2016-12-05 17:51:05 +0100

    Add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 
   root 'users#new'
   
-  #get 'static_pages/home'
-  #get 'static_pages/help'
-  #get 'static_pages/about'
 
 end
