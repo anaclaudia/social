143c6cdb935bda7ef8ee0e4ede4e8821b6fec709
Ana Claudia <ana@anaclaudia.net.br>
2016-12-06 15:09:14 +0100

    Add has_secure_password on model, and add password fields on form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
 
-#  attr_accessor :firstname, :lastname, :website
-
   validates(:firstname, presence: true, length: { maximum: 15 })
   validates(:lastname, presence: true, length: { maximum: 20 })
 
+  has_secure_password
 end
