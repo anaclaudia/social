6c48b150552bbd1ee1c382f21041ed430ca4ffbd
Ana Claudia <ana@anaclaudia.net.br>
2016-12-06 16:12:46 +0100

    Add validation for password and website fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
 
   validates(:firstname, presence: true, length: { maximum: 15 })
   validates(:lastname, presence: true, length: { maximum: 20 })
+  validates(:password, presence: true, length: { minimum: 6 })
+  validates(:password_confirmation, presence: true)
+  validates(:password, confirmation: true)
+  validates(:website, presence: true)
 
   has_secure_password
 end
