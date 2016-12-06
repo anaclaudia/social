b489071c04a463c7026c78e4df6138991cc3dcd8
Ana Claudia <ana@anaclaudia.net.br>
2016-12-06 19:36:40 +0100

    Add create method, to permit login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,17 @@ class SessionsController < ApplicationController
   def new
     render 'new'
   end
+
+  def create
+    user = User.find_by(website: params[:session][:website].downcase)
+    if 
+      user && user.autenticate(params[:session][:password])
+    else
+      flash[:danger] = "Invalid email or password!"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
