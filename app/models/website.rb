e6c8f9fc3629e3a28daedf32fa7f875f475f3c02
Ana Claudia <ana@anaclaudia.net.br>
2016-12-15 21:24:08 +0100

    Add website fetcher model

---
+++ app/models/website.rb
@@ -0,0 +1,19 @@
+class Website
+  def self.parse(page_url)
+    unless page_url.include? "http://"
+      page_url.insert(0, 'http://') 
+    end
+
+    begin
+      page = Nokogiri::HTML(open(page_url, :allow_redirections => :all))
+      links =  page.css("body").css("a")
+      
+      links.map do |link|
+        link.text
+      end
+    rescue => e
+      error_msg = "Website not found, or URL is incomplete!"
+      []
+    end
+  end
+end
