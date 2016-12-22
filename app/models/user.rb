ea6168d2b8c08dd5273216085f17a2768634ae2b
Ana Claudia <ana@anaclaudia.net.br>
2016-12-22 12:05:51 +0100

    Adding posts column to user's table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   
+  serialize :posts, Array
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   
---
+++ db/migrate/20161222110305_add_posts_to_user.rb
@@ -0,0 +1,5 @@
+class AddPostsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :posts, :text
+  end
+end
